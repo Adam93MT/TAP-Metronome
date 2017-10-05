bdf09967d6d3a0d32008c896f26ac44622f3bf77
adam <adam@adamthompson.ca>
2017-10-04 21:47:28 -0400

    Fixed some timer bugs and got the UI going nice

---
+++ Metronome-Swift/CustomHeightSlider.swift
@@ -0,0 +1,56 @@
+//
+//  CustomHeightSlider.swift
+//  TAP Metronome-Swift
+//
+//  Created by Adam M Thompson on 2017-10-03.
+//  Copyright © 2017 Adam Thompson. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeightSlider: UISlider {
+    
+    let sliderMinColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    let sliderMaxColor = UIColor.black//UIColor(red:0.04, green: 0.04, blue: 0.04, alpha: 1)
+    let sliderThumbColor = UIColor(red: 0.706, green: 0.706, blue: 0.706, alpha: 1)
+    let trackHeight: CGFloat = 64
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("init Slider")
+        self.setup()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)!
+        print("init Slider")
+        self.setup()
+    }
+    
+    override func trackRect(forBounds bounds: CGRect) -> CGRect {
+            var newBounds = super.trackRect(forBounds: bounds)
+            newBounds.size.height = self.trackHeight
+            newBounds.size.width = self.frame.width
+            return newBounds
+    }
+    
+    private func setup() {
+        self.thumbRect(
+            forBounds: CGRect(x:0, y:0, width:self.trackHeight, height: self.trackHeight),
+            trackRect: CGRect(x:0, y:0, width:self.trackHeight, height: self.trackHeight),
+            value: Float(self.trackHeight)
+        )
+        let _ = self.trackRect(
+            forBounds: CGRect(x:0, y:0, width:self.trackHeight, height: self.trackHeight)
+        )
+        print("H: \(self.frame.height), W: \(self.frame.width)")
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Metronome-Swift/MetronomeViewController.swift
+++ Metronome-Swift/MetronomeViewController.swift
@@ -14,24 +14,24 @@ class MetronomeViewController: UIViewController {
     
     @IBOutlet weak var tempoLabel: UILabel!
     @IBOutlet weak var tapButton: UIButton!
-    @IBOutlet weak var tempoSlider: UISlider!
     @IBOutlet weak var timeSignatureButton: UIButton!
-
-     // The superView containing all animating Circles
+    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var tempoSlider: CustomHeightSlider!
+    
+    // The superView containing all animating Circles
     //@IBOutlet weak var beatCircleView: BeatCircleView!
-    var containerView: BeatContainerView!
     
     let metronome = Metronome()
+    var containerView: BeatContainerView!
     var metronomeDisplayLink: CADisplayLink!
     
-    
     // Colours
-    let backgroundColor = UIColor(red:0.04, green: 0.04, blue: 0.04, alpha: 1)
-    let sliderMinColor = UIColor(red: 0.353, green: 0.353, blue: 0.353, alpha: 1)
-    let sliderMaxColor = UIColor(red: 0.211, green: 0.211, blue: 0.211, alpha: 1)
-    let sliderThumbColor = UIColor(red: 0.706, green: 0.706, blue: 0.706, alpha: 1)
+    let backgroundColor = UIColor.black
     let tempoLabelColor = UIColor(red: 0.14, green: 0.14, blue: 0.14, alpha: 1)
     let tempoLabelColorPressed = UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1)
+    let MinColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.2)
+    let MaxColor = UIColor(red:0.04, green: 0.04, blue: 0.04, alpha: 0.3)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,23 +41,39 @@ class MetronomeViewController: UIViewController {
         let viewWidth = view.frame.width
         let viewHeight = view.frame.height
         print("Height: \(viewHeight), Width: \(viewWidth)")
+        
+        // Set Up Beat Circle Views
         self.containerView = BeatContainerView(frame: CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight))
         self.containerView.center = view.center
+        self.containerView.backgroundColor = UIColor.clear
         view.addSubview(self.containerView)
-//        view.sendSubview(toBack: self.containerView)
+        view.sendSubview(toBack: self.containerView)
         
+        // Set up Tap button
         tapButton.frame.size = CGSize(width: viewWidth, height: viewWidth);
         tapButton.contentEdgeInsets = UIEdgeInsetsMake(viewWidth/2, viewWidth/2, viewWidth/2, viewWidth/2);
         view.bringSubview(toFront: tapButton)
-        
         view.backgroundColor = backgroundColor
         
+        // Set up Tempo Control Buttons, Slider and Text
         tempoLabel.text = String(metronome.tempo)
+        incrementButton.backgroundColor = MaxColor
+        decrementButton.backgroundColor = MinColor
+        tempoSlider.thumbTintColor = UIColor.clear
+        tempoSlider.minimumTrackTintColor = MinColor
+        tempoSlider.maximumTrackTintColor = MaxColor
+        tempoSlider.setMinimumTrackImage(UIImage(named: "sliderCapMin"), for: UIControlState.normal)
+        tempoSlider.setMaximumTrackImage(UIImage(named: "sliderCapMax"), for: UIControlState.normal)
+        tempoSlider.maximumValue = Float(metronome.maxTempo)
+        tempoSlider.minimumValue = Float(metronome.minTempo)
         tempoSlider.value = Float(metronome.tempo)
-        tempoSlider.thumbTintColor = sliderThumbColor
-        tempoSlider.minimumTrackTintColor = sliderMinColor
-        tempoSlider.maximumTrackTintColor = sliderMaxColor
-        tempoSlider.isEnabled = true
+        
+//        sliderIncrement.maximumValue = Float(metronome.maxTempo)
+//        sliderIncrement.minimumValue = Float(metronome.minTempo)
+//        tempoSlider.thumbTintColor = sliderThumbColor
+//        tempoSlider.minimumTrackTintColor = sliderMinColor
+//        tempoSlider.maximumTrackTintColor = sliderMaxColor
+//        tempoSlider.isEnabled = true
 
         metronome.prepare()
         
@@ -68,27 +84,30 @@ class MetronomeViewController: UIViewController {
         
     }
     
-    @IBAction func tempoSliderChanged(_ sender: UISlider) {
-        print("Slider Value: \(tempoSlider.value)")
-        metronome.setTempo(newTempo: Int(tempoSlider.value))
-        tempoLabel.text = String(metronome.tempo)
-        metronome.prepare()
-    }
-    
     @IBAction func swipeButton(_ sender: UIButton) {
         if metronome.isOn { self.stopMetronome() }
-        // else ignore
-        
     }
     
     @IBAction func tapDown(_ sender: UIButton) {
-//        metronome.logTap()
         if metronome.isOn { metronome.logTap() }
         else { self.startMetronome() }
         
     }
     @IBAction func tapUp(_ sender: UIButton) {
-        
+        // nothing
+    }
+    
+    @IBAction func incrementButtonPressed(_ sender: UIButton) {
+        self.metronome.incrementTempo()
+    }
+    
+    @IBAction func decrementButtonPressed(_ sender: UIButton) {
+        self.metronome.decrementTempo()
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("Slider Value: \(Int(tempoSlider.value))")
+        self.metronome.setTempo(newTempo: Int(tempoSlider.value))
     }
     
     func toggleMetronome() {
@@ -113,19 +132,19 @@ class MetronomeViewController: UIViewController {
     func startUI() {
         //        tempoSlider.isEnabled = false
         tempoLabel.isEnabled = false
-        UIView.animate(withDuration: 0.2, animations: {() -> Void in
-            self.tempoSlider.alpha = 0.4
-        })
+//        UIView.animate(withDuration: 0.2, animations: {() -> Void in
+//            self.tempoSlider.alpha = 0.4
+//        })
     }
     
     func stopUI() {
         tapButton.isEnabled = true
         tapButton.isHidden = false
         // Enable the metronome stepper.
-        tempoSlider.isEnabled = true
-        UIView.animate(withDuration: 0.2, animations: {() -> Void in
-            self.tempoSlider.alpha = 1
-        })
+//        tempoSlider.isEnabled = true
+//        UIView.animate(withDuration: 0.2, animations: {() -> Void in
+//            self.tempoSlider.alpha = 1
+//        })
     }
     
     // MARK: - UIResponder
