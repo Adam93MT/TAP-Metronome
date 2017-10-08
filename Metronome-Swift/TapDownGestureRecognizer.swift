a44e32e465a40c49ba2dfae367fc996bd1b688d7
adam <adam@adamthompson.ca>
2017-10-08 12:30:58 -0400

    Added thumb signifier, and implemented touch location tracking

--- Metronome-Swift/TapDownGestureRecognizer.swift
+++ Metronome-Swift/TapDownGestureRecognizer.swift
@@ -6,18 +6,22 @@
 //  Copyright © 2017 Adam Thompson. All rights reserved.
 //
 
+import UIKit
 import UIKit.UIGestureRecognizerSubclass
 
 class TapDownGestureRecognizer: UITapGestureRecognizer {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesBegan(touches, with: event)
         if self.state == .possible {
             self.state = .recognized
         }
     }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+//        super.touchesMoved(touches, with: event) // Dont inherit super
         self.state = .failed
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
-        self.state = .failed
+//        super.touchesEnded(touches, with: event) // Dont inherit super
+        self.state = .failed // fail on touch up
     }
 }
