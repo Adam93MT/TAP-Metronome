86583c7bb5ca966ff28fafe6cca5f63dea9fb26d
Adam <adam.m.thompson@icloud.com>
2017-10-16 23:21:58 -0400

    Got the Modal view working with close button

---
+++ Metronome-Swift/TempoSliderViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TempoSliderViewController.swift
+//  TAP Metronome-Swift
+//
+//  Created by Adam M Thompson on 2017-10-16.
+//  Copyright © 2017 Adam Thompson. All rights reserved.
+//
+
+import UIKit
+
+class TempoSliderViewController: UIViewController {
+    
+    @IBOutlet weak var closeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.clear
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.frame
+//
+        self.view.insertSubview(blurEffectView, at: 0)
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+//        closeButton.center.y += 64
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        closeButton.center.y -= 64
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+     @IBAction func closeTempoModal(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+     }
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+}
